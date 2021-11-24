87fb89eabfcebf91bb33bb2986bdab9c160e6bf7
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2021-11-23 20:56:59 -0800

    removed axios

--- src/components/create/Expense.js
+++ src/components/create/Expense.js
@@ -1,6 +1,6 @@
 // import handleChange from "./CreateBudget"
 import { useState } from "react";
-import axios from "axios";
+
 function Expense(props) {
   const [expenseName, setExpenseName] = useState("");
   const [expenseAmount, setExpenseAmount] = useState("");
